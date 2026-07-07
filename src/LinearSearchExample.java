bc50c9486ad4692a076e950a80c96d448b57ef38
Tejas14@ <tejaspathak1412@gmail.com>
2026-07-08 02:33:46 +0530

    Added today's DSA practice

---
+++ src/LinearSearchExample.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LinearSearchExample {
+    static boolean findTarget(int[] arr, int target){
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == target){
+                return true;
+            }
+        }
+        //if not then false
+        return false;
+    }
+
+
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter arr size:");
+        int n = sc.nextInt();
+//        System.out.println("enter arr size:");
+        int [] arr = new int[n];
+        System.out.println("enter array element :");
+//        boolean ans = findTarget(arr, 9);
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        boolean ans = findTarget(arr, 9);
+        System.out.println(ans);
+
+    }
+}
