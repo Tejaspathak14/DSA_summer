8e8dc1adc2dadeb127107cf93f8451606bcb6f4e
Tejas14@ <tejaspathak1412@gmail.com>
2026-07-11 00:32:10 +0530

    Initial commit

---
+++ src/PosNegsum.java
@@ -0,0 +1,24 @@
+public class PosNegsum {
+    static int[] getPosNegSum(int[] arr){
+        int Possum = 0;
+        int Negsum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>0){
+                Possum = Possum + arr[i];
+            }
+            else {
+                Negsum = Negsum + arr[i];
+            }
+        }
+        int ans[] = {Possum , Negsum};
+        return ans;
+    }
+
+
+    static void main() {
+        int arr[] = {2,-1,5,-15,23};
+        int ans[] = getPosNegSum(arr);
+        System.out.println("Possum :"+ans[0]);
+        System.out.println("NegSum :"+ans[1]);
+    }
+}
